6652e0cabe4235693a3af854476027e64faeb2bb
Anastasiya Mar <nastenkammmggg@gmail.com>
2024-10-09 22:50:41 +0300

    added nedotask4

---
+++ task4/Program.cs
@@ -0,0 +1,43 @@
+﻿
+Console.WriteLine("Vvedi naturalnoye chislo");
+int num = Convert.ToInt32(Console.ReadLine());
+int count=1;
+int helper=num;
+while (helper>=10)
+{
+    helper=helper/10;
+    count=count+1;
+}
+if (count==1)
+{
+    Console.WriteLine($"{num} - yedinstvennaya cifra");
+}
+else
+{
+  int count2=0;
+  int helper2=num;
+  stepen10=count-1
+
+   int array[]= cifri[];
+
+   while (count2<=num-1)//0>=posledn index
+   {
+    while(stepen10>=1)
+    {
+        int a=10
+    }
+    cifri[count2]=helper2/
+    
+    array[count2]=
+   }
+   
+   
+   
+   
+   count=count-1;
+   int count2=0;
+    while (count2<count)
+    {
+      Console.WriteLine()  
+    }
+}
