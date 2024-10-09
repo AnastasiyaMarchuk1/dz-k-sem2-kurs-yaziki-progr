e5fd4766eeff057ba90d8d8d1e069e1193df2c7e
Anastasiya Mar <nastenkammmggg@gmail.com>
2024-10-09 22:03:52 +0300

    added task3

---
+++ task3/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Vvedi dvuznachnoye chislo:");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num>=10 && num<=99)
+{
+  int firstdigit=num/10;
+  int seconddigit=num%10;
+  if (firstdigit>seconddigit)
+  {
+    Console.WriteLine("Pervaya cifra bolshe vtoroy");
+  }  
+  else if (firstdigit<seconddigit)
+  {
+    Console.WriteLine("Pervaya cifra menshe vtoroy");
+  }
+  else
+  {
+  Console.WriteLine("Pervaya i vtoraya cifri ravny");  
+  }  
+  }
+else
+{
+    Console.WriteLine("Chislo ne prinadlezhit zadannomu promezhutku");
+}
