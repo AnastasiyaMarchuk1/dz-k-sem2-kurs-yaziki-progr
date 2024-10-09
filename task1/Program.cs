d4a7cc800cfde8bfda34e424108b835c6844cbd0
Anastasiya Mar <nastenkammmggg@gmail.com>
2024-10-09 21:50:51 +0300

    added task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,8 +1,11 @@
 ﻿Console.WriteLine("Введи число:");
 int num = Convert.ToInt32(Console.ReadLine());
-if (num % 7 == 0 && num % 23 = 0)
+if (num % 7 == 0) 
 {
+       if (num % 23 == 0)
+       {
         Console.WriteLine($"Число {num} одновременно кратно и 7, и 23.");
+       } 
 }
 else
 {
